d80e1efe75492125efaa40ed6a4bad1b4288514b
Anton Volokha <anton.v@seed4.me>
2020-03-16 12:20:33 +0200

    Add comments

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-keyFileName = 'BaDMaN.key'
-keyValue = u'wW9dQQ8vjL0Z_ldMyqH9dgePMKJNNK25VrY7BKPCYsE='
+keyFileName = 'BaDMaN.key' #default name of key files
+keyValue = u'wW9dQQ8vjL0Z_ldMyqH9dgePMKJNNK25VrY7BKPCYsE=' #put here your default value if you want
